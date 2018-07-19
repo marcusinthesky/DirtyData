bf09bb00b5b990db71db83d5f3ad30412c57cf41
Marcus Gawronsky <gwrmar002@myuct.ac.za>
2018-07-19 15:49:19 +0200

    New post.

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,7 +33,7 @@ SOCIAL = (('Twitter', 'https://twitter.com/marcusinthesky'),
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
+RELATIVE_URLS = False
 
 MARKUP = ('md', 'ipynb')
 
